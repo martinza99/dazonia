502031597c01d795d334e3c296f90fb0153190ec
Martin Za <zacharias.m@outlook.de>
2019-05-14 17:17:38 +0200

    fixed links in header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
-<?
-require_once "sql.php";
+<?php
+require_once "login/sql.php";
 echo "
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">
@@ -12,11 +12,11 @@ echo "
 <nav class=\"navbar navbar-inverse\">
 <div class=\"container-fluid\">
     <div class=\"navbar-header\">
-        <a class=\"navbar-brand\" href=\"$domain\" target=\"_top\"><img src=\"/logo.png\"></a>
+        <a class=\"navbar-brand\" href=\"$domain\" target=\"_top\"><img src=\"/logo.png\" style=\"margin-top: -12px;\"></a>
     </div>
     <ul class=\"nav navbar-nav\">
-        <li><a href=\"$domain/list\" target=\"_top\">List</a></li>
-        <li><a href=\"#\" target=\"_top\">PFPs</a></li>
+        <li><a href=\"$domain/list\" target=\"_top\">List View</a></li>
+        <li><a href=\"#\" target=\"_top\">Prof Pics</a></li>
         <li><a href=\"#\" target=\"_top\">Safe Mode</a></li>
     </ul>
     <form class=\"navbar-form navbar-left\" action=\"$domain/list.php\" method=\"GET\" autocomplete=\"off\">
