dd3b26228296e2e09f14e0e1ccd8072b8e963d41
Martin Za <zacharias.m@outlook.de>
2019-05-20 05:05:11 +0200

    removed domain prefix from favicon

--- header.php
+++ header.php
@@ -5,7 +5,7 @@ echo "
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">
 <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js\"></script>
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script>
-<link rel=\"shortcut icon\" href=\"favicon.png\" type=\"image/x-icon\">
+<link rel=\"shortcut icon\" href=\"/favicon.png\" type=\"image/x-icon\">
 </head>
 <body>
 
