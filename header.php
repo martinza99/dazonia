3ecb5232c6207fb204521ac0db464d57e9b748e2
Martin Za <zacharias.m@outlook.de>
2019-05-20 20:24:19 +0200

    added version hash to header

--- header.php
+++ header.php
@@ -6,7 +6,10 @@ echo "
 <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js\"></script>
 <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js\"></script>
 <link rel=\"shortcut icon\" href=\"/favicon.png\" type=\"image/x-icon\">
-</head>
+</head>";
+exec('git rev-parse --verify HEAD', $output);
+echo "<!--$output[0]-->";
+echo "
 <body>
 
 <nav class=\"navbar navbar-inverse\">
