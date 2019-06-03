682a7a67c4252090c649226fbf7c9cfa9effd8ed
Martin Za <zacharias.m@outlook.de>
2019-06-03 18:33:51 +0200

    added server_info.php for debugging

---
+++ login/server_info.php
@@ -0,0 +1,17 @@
+<?php
+    session_start();
+    require_once "sql.php";
+    if($_SESSION["userId"]>1||!checkLogin($_SESSION["userId"])){
+        header("Location: .");
+        die();
+    }
+    $userId = $_SESSION["userId"];
+
+    echo "<table border=\"1\">";
+
+    foreach($_SERVER as $valueType=>$value){
+        echo "<tr><td>$valueType</td><td>$value</td></tr>";
+    }
+    echo "</table>";
+
+?>
