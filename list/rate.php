7908922423ccc4a4596cd177936bd703f4e435f0
Martin Za <zacharias.m@outlook.de>
2019-06-03 20:27:15 +0200

    fixed rate.php response

--- list/rate.php
+++ list/rate.php
@@ -26,11 +26,11 @@
     $sql = $conn->prepare("SELECT AVG(userrating.rating) AS avgrating FROM userrating WHERE fileId = ?");
     $sql->bind_param("s",$fileid);
     $sql->execute();
-    $rating = mysqli_fetch_assoc($sql->get_result());
-    if(!isset($rating["avgrating"]))
-        $rating["avgrating"] = 0;
+    $rating = mysqli_fetch_assoc($sql->get_result())["avgrating"];
+    if(!isset($rating))
+        $rating = 0;
     if($rating - floor($rating) == 0.5)
         $rating = floor($rating);
     $rating = (int) $rating;// 5.000 -> 5
-    echo $rating["avgrating"];
+    echo $rating;
 ?>
