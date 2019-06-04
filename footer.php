af30f155f70eceb54bae489c336aa1fa30ffe5b6
Martin Za <zacharias.m@outlook.de>
2019-06-04 05:34:08 +0200

    fixed update button on upload page

--- footer.php
+++ footer.php
@@ -34,7 +34,7 @@ echo "
 <div class=\"right bottom\">";
 $temp = "";
 if($_SERVER["SCRIPT_NAME"]=="/upload/index.php")
-    $temp = "login/";
+    $temp = "../login/";
     if($userId<2)//update server button
         echo "
             <form action=\"".$temp."remote.php\" method=\"POST\" autocomplete=\"off\">
