f48ac3bb6e781fdbac13221b21a8b61cd89d97ae
Martin Za <zacharias.m@outlook.de>
2019-06-04 05:21:25 +0200

    updated userscript

--- fixMasking.user.js
+++ fixMasking.user.js
@@ -1,18 +1,20 @@
 // ==UserScript==
-// @name         dazoniaFixMaskin
+// @name         dazoniaFixMasking
 // @namespace    https://github.com/martinza99/dazonia
-// @version      0.2
+// @version      0.3
 // @description  fixes domain masking stuff
 // @author       martin
 // @match        http://dazonia.xyz/*
+// @include      http://*.dazonia.xyz/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    'use strict';
-    let link = document.createElement("link");
-    link.rel = "shortcut icon";
-    link.href = "https://ma.2ix.ch/favicon";
-    link.type = "image/x-icon";
-    document.querySelector("head").appendChild(link);
-});
+var favicon_link_html = document.createElement('link');
+favicon_link_html.rel = 'icon';
+favicon_link_html.href = "https://ma.2ix.ch/favicon";
+favicon_link_html.type = 'image/x-icon';
+
+try {
+  document.getElementsByTagName('head')[0].appendChild( favicon_link_html );
+}
+catch(e) { }
