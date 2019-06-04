b4ebad4c632e7ef40ee037773862283f7ea49498
Martin Za <zacharias.m@outlook.de>
2019-06-04 04:45:22 +0200

    added js click on link to fix domain masking

--- list/search.php
+++ list/search.php
@@ -1,7 +1,14 @@
 <?php
     require_once '../login/sql.php';
     require_once '../login/functions.php';
-    echo $_SERVER[QUERY_STRING];
-    header("Location: $domain/list/?$_SERVER[QUERY_STRING]");
+    echo "<body onload=\"clickLink();\">
+        <a href=\"$domain/list/?$_SERVER[QUERY_STRING]\" target=\"_top\" class=\"link\" style=\"opacity:0;\">$domain/list/?$_SERVER[QUERY_STRING]</a>
+        <script>
+            function clickLink(){
+                document.querySelector(\".link\").click();
+            }
+        </script>
+    </body>";
     die();
 ?>
+
