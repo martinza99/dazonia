05a39e39fa23c0cccf523ef9bc1aeadeca0a2c1f
Martin Za <zacharias.m@outlook.de>
2019-06-07 03:37:48 +0200

    added console and output on remote page

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
-    $hash = time();
 echo "
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css\">
--- login/remote.php
+++ login/remote.php
@@ -5,34 +5,82 @@
     if($_SESSION["userId"]>1||!checkLogin($_SESSION["userId"])){
         header("Location: .");
         die();
-    }
+    }    
+    if(isset($_POST["action"]))
+        $action = $_POST["action"];
+    else
+        $action = "";
     $userId = $_SESSION["userId"];
-
+    $result;
     if(isset($_POST["action"])){
+        $sql = $_POST["sql"];
         switch ($_POST["action"]){
             case "sql": 
-                $sql = $_POST["sql"];
                 $result = $conn->query($sql);
                 break;
             case "u":
                 exec('git pull https://github.com/martinza99/dazonia.git master');
                 header("Location: remote.php");
                 break;
+            case "cmd":
+                exec($sql,$outputExec);
+                $result = $outputExec[0];
+                break;
+            case "r":
+                exec("");
         }
     }
     require_once "../header.php";
+
 ?>
 
     <title>Remote Server Settings</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <form action="remote.php" method="POST" autocomplete="off">
-        <input type="hidden" name="action" value="sql">
-        <textarea name="sql" cols="50" rows="5" placeholder="SQL query"></textarea><br>
-        <input type="submit" value="Submit Query">
+    <form action="remote.php" method="POST" autocomplete="off" class="queryForm">
+        <input type="hidden" name="action" class="formAction">
+        <textarea name="sql" cols="50" rows="5" placeholder="query"><?php if(!empty($action))echo $sql ?></textarea><br>
+        <input type="button" value="Submit SQL" onclick="setForm('sql');">
+        <input type="button" value="Run CMD" onclick="setForm('cmd');">
     </form>
-    
+    <script>
+        function setForm(_value){
+            document.querySelector(".formAction").value = _value;
+            document.querySelector(".queryForm").submit();
+        }
+    </script>
+    <div class="result" style="color:black;">
+       <?php
+            if($action=="cmd"){
+                echo "<pre>";
+                print_r($outputExec);
+                echo "</pre>";
+            }
+        else if($action=="sql"&&!$result){
+            echo "<pre>";
+            print_r($conn->error);
+            echo "</pre>";
+        }else if($action=="sql"){
+                echo "<table border=\"1\">";
+                $names = $result->fetch_assoc();
+                    echo "<tr>";
+                    foreach($names as $key => $value)
+                        echo "<th>$key</th>";
+                    echo "</tr><tr>";
+                    foreach($names as $value)
+                        echo "<td>$value</td>";
+                    echo "<tr>";
+                while($rows = $result->fetch_assoc()){
+                    echo "<tr>";
+                    foreach($rows as $value)
+                        echo "<td>$value</td>";
+                    echo "<tr>";
+                }
+                echo "</table>";
+            }
+        ?>
+    </div>
 <?php
     require_once "../footer.php"; 
 ?>
