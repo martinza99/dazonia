bf0517def50af1b140136e84eb41a62fa11aff1d
Martin Za <zacharias.m@outlook.de>
2019-06-27 19:38:27 +0200

    password reset now also resets the API key

--- login/resetPassword.php
+++ login/resetPassword.php
@@ -4,10 +4,10 @@
     require_once 'functions.php';
     require_once "../header.php";
 
-    if(!isset($_GET["resetKey"])&&(!isset($_SESSION["userId"])||!checkLogin($_SESSION["userId"]))){
-        header("Location: $domain/login");
-        die();
-    }
+    // if(!isset($_GET["resetKey"])&&(!isset($_SESSION["userId"])||!checkLogin($_SESSION["userId"]))){
+    //     header("Location: $domain/login");
+    //     die();
+    // }
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +21,13 @@
 </head>
 <body>
 	<form id="login" action="resetPassword.php" method="post" class="center">
-        <?php if(!isset($_GET["resetKey"])) echo '<input type="password" placeholder="current password" name="currentPassword" autocomplete="current-password" required><br>';?>
+        <?php
+        if(isset($_GET["resetKey"])){//warning icon or current password field
+            echo "<span class=\"glyphicon glyphicon-exclamation-sign\" style=\"color:red;\"></span><span>Your API key will be reset</span><br>";
+        }else{
+            echo "<input type=\"password\" placeholder=\"current password\" name=\"currentPassword\" autocomplete=\"current-password\" required><br>";
+        }
+        ?>
 		<input type="password" placeholder="new password" name="newPassword" id="password" oninput="equals(this, '#passwordConfirm');" autocomplete="new-password" required><br>
         <input type="password" placeholder="confirm password" autocomplete="new-password" id="passwordConfirm" oninput="equals(this, '#password');" autocomplete="new-password" required><br>
         <?php if(isset($_GET["resetKey"])) echo "<input type=\"hidden\" name=\"resetKey\" value=\"$_GET[resetKey]\">";?>
@@ -43,15 +49,20 @@
             $changePass = password_verify($currentPassword,$hash);
         }
         else if(isset($_POST["resetKey"])){
-            $resetKey = $_POST["resetKey"];
             $sql = $conn->prepare("SELECT id FROM users WHERE apiKey = ?");
-            $sql->bind_param("s", $resetKey);
+            $sql->bind_param("s", $_POST["resetKey"]);
             $sql->execute();
             $result = $sql->get_result();
             if($result->num_rows>0){
                 $userid = $result->fetch_assoc()["id"];
                 $changePass = true;
                 $_SESSION["userId"] = $userid;
+
+                //reset API key
+                $apiKey = generateRandomString(64);
+                $sql = $conn->prepare("UPDATE users SET apiKey = ? WHERE id = ?");
+                $sql->bind_param("si",$apiKey,$userid);
+                $sql->execute();
             }
         }
 
@@ -62,13 +73,28 @@
             if(!$sql->execute()){
                 die("SQL error! - Please contact an admin.");
             }
+
             echo "successfully changed password";
         }
         else{
             echo "Wrong password!";
         }
     }
+    if(isset($_SESSION["userId"]))
+        require "../footer.php";
 ?>
 
 </body>
 </html>
+
+<?php
+function generateRandomString($length){//generates random strings
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ-_';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+?>
