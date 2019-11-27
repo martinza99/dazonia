ebf1fe15847371d36effd17ef60ca1e707c0a092
Martin Za <pickolo04@gmail.com>
2019-11-27 01:34:55 +0100

    testing disabled datalist

--- login/functions.php
+++ login/functions.php
@@ -1,40 +1,42 @@
 <?php
 
-	if(isset($_COOKIE["noMasking"]) && $_COOKIE["noMasking"] == "true")
-		$domain = "";
+if (isset($_COOKIE["noMasking"]) && $_COOKIE["noMasking"] == "true")
+	$domain = "";
 
-    exec('git rev-parse --verify HEAD', $output);//requires git
-    $hash = substr($output[0],0,6);
-    
-    function checkLogin($userID){
-        $conn = $GLOBALS["conn"];
-        $sql = $conn->prepare("SELECT * FROM users WHERE id = ?");
-        $sql->bind_param("i",$userID);
-        $sql->execute();
-        if($sql->get_result()->num_rows==0){
-            session_destroy();
-            return false;
-        }
-        return true;
-    }
+exec('git rev-parse --verify HEAD', $output); //requires git
+$hash = substr($output[0], 0, 6);
 
-    function prePrint($var){
-        echo "<pre>";
-        print_r($var);
-        echo "</pre>";
-    }
+function checkLogin($userID)
+{
+	$conn = $GLOBALS["conn"];
+	$sql = $conn->prepare("SELECT * FROM users WHERE id = ?");
+	$sql->bind_param("i", $userID);
+	$sql->execute();
+	if ($sql->get_result()->num_rows == 0) {
+		session_destroy();
+		return false;
+	}
+	return true;
+}
 
-    function printDatalistTags(){
-        if(isset($_COOKIE["showTaglist"]) && $_COOKIE["showTaglist"] == "true"){
-			$conn = $GLOBALS["conn"];
-			echo '<datalist id="tagList">';
-			$sql = $conn->prepare("SELECT name FROM tags");
-			$sql->execute();
-			$result = $sql->get_result();
-			while($rows = $result->fetch_assoc()){
-				echo "<option value=\"$rows[name]\">";
-			}
-			echo '</datalist>';
-		}
-    }
-?>
+function prePrint($var)
+{
+	echo "<pre>";
+	print_r($var);
+	echo "</pre>";
+}
+
+function printDatalistTags()
+{
+	// if (isset($_COOKIE["showTaglist"]) && $_COOKIE["showTaglist"] == "true") {
+	$conn = $GLOBALS["conn"];
+	$sql = $conn->prepare("SELECT name FROM tags ORDER BY name");
+	$sql->execute();
+	$result = $sql->get_result();
+	echo '<datalist id="tagList">';
+	while ($rows = $result->fetch_assoc()) {
+		echo "<option value=\"$rows[name]\" disabled>";
+	}
+	echo '</datalist>';
+	// }
+}
