b341fc76fb8138dd08e0d47f22b473f4a234d77e
martinza99 <mrt.zacharias@gmail.com>
2020-06-11 03:22:57 +0200

    removed commit selection from update page

--- login/update.php
+++ login/update.php
@@ -5,8 +5,7 @@
     checkAdmin();
 
     require_once "../header.php";
-    if(isset($_POST["commit"]))
-        exec("git reset --hard $_POST[commit] 2>&1", $execOutput, $exitCode);
+    exec("git pull https://github.com/martinza99/dazonia.git master 2>&1", $execOutput, $exitCode);
 ?>
 
 
@@ -14,19 +13,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    <form action="update.php" method="post" autocomplete="off">
-        <label for="commit">Commit: </label><input type="text" name="commit" id="commit" placeholder="commit" value="master" size="40"><br>
-        <input type="submit" value="Change version">
-    </form>
    <?php
-   if(isset($_POST["commit"])){
         echo "<ul>";
         foreach ($execOutput as $key => $value) {
             echo "<li>" . htmlspecialchars($value) . "</li>";
         }
         echo "<li>Exit code: $exitCode</li>";
         echo "</ul>";
-   }
         require_once("../footer.php");
    ?>
 </body>
