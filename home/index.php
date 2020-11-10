dae550d90edc2c7cc2f510a0ed5b1bf0de5daf41
martinza99 <mrt.zacharias@gmail.com>
2020-11-10 14:31:29 +0100

    moved homepage

---
+++ home/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+require_once "../login/sql.php";
+require_once "../login/functions.php";
+
+checkLogin();
+
+$p = $_GET["p"] ?? 1;
+$p = intval($p);
+if ($p < 1) {
+	$p = 1;
+}
+$offset = 100 * ($p - 1);
+
+if (isset($_GET["q"]))
+	$q = htmlspecialchars($_GET["q"]);
+else
+	$q = "safe";
+
+if (empty($q)) {
+	$sql = $conn->prepare("SELECT file.filename, ROUND(AVG(userrating.rating)) AS avgrating FROM file NATURAL LEFT JOIN userrating GROUP BY file.fileID ORDER BY fileID DESC LIMIT 100 OFFSET :offset");
+} else {
+	$sql = $conn->prepare("SELECT file.filename, ROUND(AVG(userrating.rating)) AS avgrating FROM file NATURAL LEFT JOIN userrating NATURAL JOIN tagfile NATURAL JOIN tag WHERE tag.tagname = :tagname GROUP BY file.fileID ORDER BY fileID DESC LIMIT 100 OFFSET :offset");
+	$sql->bindValue(":tagname", $q, PDO::PARAM_STR);
+}
+$sql->bindValue(":offset", $offset, PDO::PARAM_INT);
+$sql->execute();
+
+include("./template.php");
+$sql = null;
---
+++ home/template.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<?php include("../include/head.php"); ?>
+	<title>Dazonia Home</title>
+	<link rel="stylesheet" href="/static/home/home.css">
+</head>
+
+<body>
+	<?php include("../include/nav.php"); ?>
+	<main class="potato">
+		<?php while ($file = $sql->fetchObject()) :
+			list($width, $height) = getimagesize($UPLOADS . "/thumbnails/$file->filename");
+		?>
+			<a href="<?= $CDN ?>/files/<?= $file->filename ?>?q=<?= $q ?>">
+				<div class="pics picsBorder" id="<?= $file->filename ?>">
+					<?php if (substr($file->filename, -4) == ".gif") : ?>
+						<button class="thumbButton sideView">►</button>
+					<?php endif;
+					if ($file->avgrating) : ?>
+						<img class="starView" src="/static/list/img/<?= $file->avgrating ?>.png" alt="<?= $file->avgrating ?>">
+					<?php endif; ?>
+					<img class="thumb" src="<?= $CDN ?>/thumbnails/<?= $file->filename ?>" alt="<?= $file->filename ?>" loading="lazy" width="<?= $width ?>" height="<?= $height ?>">
+				</div>
+			</a>
+		<?php endwhile; ?>
+	</main>
+	<aside class="pageButtons">
+		<a href="/?p=<?= ($p - 1) ?>&q=<?= $q ?>"><button>←</button></a>
+		<span><?= $p ?></span>
+		<a href="/?p=<?= ($p + 1) ?>&q=<?= $q ?>"><button>→</button></a>
+	</aside>
+</body>
+
+</html>
